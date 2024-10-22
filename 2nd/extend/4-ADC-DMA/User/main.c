7f6f02f52b1065f160a64aa85f18f966ce244632
Bin Liu <flylb1@gmail.com>
2024-10-22 14:58:37 +0800

    update ADC DMA

--- 2nd/extend/4-ADC-DMA/User/main.c
+++ 2nd/extend/4-ADC-DMA/User/main.c
@@ -1,7 +1,6 @@
 /**
  *
- * 目标:按下 按键2(PB10) 后 5s 内 LED 闪烁，之后⾃动切换为呼吸灯模式
- * 提示:使用TIM 的定时中断功能来实现 LED 闪烁
+ * 加⼊ 反射式红外传感器 代替 按键1 ，在距离过近时启动 LED ，在距离过远时关闭 LEDƒ
  *
  */
 #include "stm32f10x.h"  // Device header
@@ -17,6 +16,7 @@ static uint8_t led_on = 1;              /*LED 开关状态*/
 static uint16_t brightness = 0;         /*LED 当前的亮度*/
 static uint8_t breathing_direction = 0; /*呼吸灯的方向*/
 static uint8_t led_mode = BREATHE;      /*LED当前的运行方式*/
+static uint16_t AD_Value = 0;
 
 void LED_Blink(void);
 
@@ -24,17 +24,19 @@ void Resource_Init(void)
 {
     /*GPIO 配置 START----------------------------------------------------------------------------------------*/
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE); /** 使能 ADC1 */
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);   /** 使能 DMA1 */
 
     /*利用宏拼接的简洁写法*/
     UTIL_GPIO_CFG_EX(A, 0, 50MHz, AF_PP); /*PA0 为复用推挽输出，用于 PWM 输出,LED*/
     UTIL_GPIO_CFG_EX(A, 1, 50MHz, AF_PP); /*PA1 为复用推挽输出，用于 PWM 输出,LED*/
+    UTIL_GPIO_CFG_EX(A, 6, 50MHz, AIN);   /*PA3 为模拟输入*/
     UTIL_GPIO_CFG_EX(B, 0, 50MHz, IPU);   /*PB0 为上拉输入，用于按键输入,BUTTON*/
     UTIL_GPIO_CFG_EX(B, 10, 50MHz, IPU);  /*PB10 为上拉输入，用于按键输入,BUTTON*/
     /*GPIO 配置 END------------------------------------------------------------------------------------------*/
 
     /*TIM 配置 START------------------------------------------------------------------------------------------*/
-    /*TIM2 属于低速定时器，时钟源来自于 APB1 总线，通过 APB1 外设时钟使能*/
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); /*TIM2 属于低速定时器，时钟源来自于 APB1 总线*/
 
     UTIL_TIM_BASE_EX(TIM2, 999, 71, DIV1, Up); /*配置定时器基本参数,目标是 TIM2*/
 
@@ -43,22 +45,49 @@ void Resource_Init(void)
     UTIL_TIM_PWM_EX(TIM2, 2, /*PA1*/ PWM1, Enable, 0, High); /*TIM2_CH2 <=> PA1*/
 
     TIM_Cmd(TIM2, ENABLE); /* 启动 TIM2*/
+    /*TIM 配置 END----------------------------------------------------------------------------------------------*/
 
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); /*使能 TIM3 时钟*/
-    /**
-     * Prescaler = 7199 将时钟频率从 72 MHz 降低到10,000Hz = (72,000,000)/(7199+1)
-     * 定时器的频率是 10 kHz（每秒钟计数 10,000 次）
-     * 为了实现 5秒 的定时，定时器需要在 5 秒 内计数 5秒×10,000Hz=50,000次
-     * 因此，自动重装载值（ARR）应设置为 50,000 - 1 = 49999。
-     */
-    UTIL_TIM_BASE_EX(TIM3, 49999, 7199, DIV1, Up); /*配置 TIM3，每 5 秒触发一次中断*/
-    TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);     /*启用 TIM3 更新中断*/
-    TIM_ClearITPendingBit(TIM3, TIM_IT_Update);    /*初始化时清除定时器中断标志，防止错误触发中断*/
-    TIM_Cmd(TIM3, DISABLE);                        /*不启动TIM3*/
-                                                   /*TIM 配置 END----------------------------------------------------------------------------------------------*/
+    /*ADC DMA初始化 START-------------------------------------------------------------------------------------------------*/
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_6, 1, ADC_SampleTime_55Cycles5); /*规则组通道配置 PA6*/
+    UTIL_ADC_CFG(ADC1,
+                 ADC_Mode_Independent,      /*_ADC_Mode ;模式，选择独立模式，即单独使用ADC1*/
+                 ENABLE,                    /*_ADC_ScanConvMode ;扫描模式，使能，扫描规则组的序列，扫描数量由ADC_NbrOfChannel确定*/
+                 ENABLE,                    /*_ADC_ContinuousConvMode; 连续转换，使能，每转换一次规则组序列后立刻开始下一次转换*/
+                 ADC_ExternalTrigConv_None, /*_ADC_ExternalTrigConv; 外部触发，使用软件触发，不需要外部触发*/
+                 ADC_DataAlign_Right,       /*_ADC_DataAlign; 数据对齐，选择右对齐*/
+                 1                          /*_ADC_NbrOfChannel; 通道数1*/
+    );
+
+    UTIL_DMA_CFG(DMA1_Channel1,
+                 (uint32_t)&ADC1->DR,             /*DMA_PeripheralBaseAddr*/
+                 (uint32_t)&AD_Value,             /*DMA_MemoryBaseAddr*/
+                 DMA_DIR_PeripheralSRC,           /*DMA_DIR*/
+                 1,                               /*DMA_BufferSize*/
+                 DMA_PeripheralInc_Disable,       /*DMA_PeripheralInc*/
+                 DMA_MemoryInc_Disable,           /*DMA_MemoryInc*/
+                 DMA_PeripheralDataSize_HalfWord, /*DMA_PeripheralDataSize*/
+                 DMA_MemoryDataSize_HalfWord,     /*DMA_MemoryDataSize*/
+                 DMA_Mode_Circular,               /*DMA_Mode*/
+                 DMA_Priority_Medium,             /*DMA_Priority*/
+                 DMA_M2M_Disable                  /*DMA_M2M*/
+    );
+
+    DMA_Cmd(DMA1_Channel1, ENABLE); /*DMA1的通道1使能*/
+    ADC_DMACmd(ADC1, ENABLE);       /*ADC1触发DMA1的信号使能*/
+    ADC_Cmd(ADC1, ENABLE);          /*ADC1使能*/
+
+    /*ADC校准*/
+    ADC_ResetCalibration(ADC1); /*固定流程，内部有电路会自动执行校准*/
+    while (ADC_GetResetCalibrationStatus(ADC1) == SET)
+        ;
+    ADC_StartCalibration(ADC1);
+    while (ADC_GetCalibrationStatus(ADC1) == SET)
+        ;
+
+    ADC_SoftwareStartConvCmd(ADC1, ENABLE); /*ADC触发*/
+                                            /*ADC DMA初始化 END ---------------------------------------------------------------------------------------------------*/
 
     /*RTC配置 START---------------------------------------------------------------------------------------------*/
-#if 1
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE); /** 启用 PWR 时钟 **/
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_BKP, ENABLE); /** 启用 BKP 时钟 **/
 
@@ -94,8 +123,6 @@ void Resource_Init(void)
         RTC_WaitForSynchro();
         RTC_WaitForLastTask();
     }
-#endif
-
     /*RTC配置 END-----------------------------------------------------------------------------------------------*/
 
     /*中断配置 START---------------------------------------------------------------------------------------------*/
@@ -127,38 +154,32 @@ void EXTI15_10_IRQHandler(void)
 {
     if (EXTI_GetITStatus(EXTI_Line10) != RESET)
     {
-        led_mode = BLINK;
+        if (led_mode == BREATHE)
+        {
+            led_mode = BLINK;
 
-        TIM_Cmd(TIM3, DISABLE);  /*停止定时器以确保重置*/
-        TIM_SetCounter(TIM2, 0); /*重置计数器为0*/
-        TIM_Cmd(TIM3, ENABLE);   /*重新启动定时器，开始新的5秒计时*/
-
-        EXTI_ClearITPendingBit(EXTI_Line10); // 清除中断标志
+            TIM_Cmd(TIM3, DISABLE);  /*停止定时器以确保重置*/
+            TIM_SetCounter(TIM2, 0); /*重置计数器为0*/
+            TIM_Cmd(TIM3, ENABLE);   /*重新启动定时器，开始新的5秒计时*/
+        }
+        else
+        {
+            led_mode = BREATHE;
+        }
     }
-}
 
-/**TIM3 中断处理*/
-void TIM3_IRQHandler(void)
-{
-    if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
-    {
-        led_mode = BREATHE;
-        TIM_Cmd(TIM3, DISABLE); // 停止定时器以确保重置
-        TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
-    }
+    EXTI_ClearITPendingBit(EXTI_Line10); // 清除中断标志
 }
 
 void LED_Blink(void)
 {
     TIM_SetCompare1(TIM2, 999); // LED1 亮
     TIM_SetCompare2(TIM2, 999); // LED2 亮
-    Delay_us(200 * 1000);       // Delay_us函数最大支持233ms,这里需要delay 250ms,分2步做delay
-    Delay_us(50 * 1000);
+    Delay_ms(250);
 
     TIM_SetCompare1(TIM2, 0); // LED1 灭
     TIM_SetCompare2(TIM2, 0); // LED2 灭
-    Delay_us(200 * 1000);
-    Delay_us(50 * 1000);
+    Delay_ms(250);
 }
 
 void Breathe_LED(void)
@@ -199,19 +220,12 @@ int main(void)
 
     while (1)
     {
-        uint32_t aa = SystemCoreClock;
+        led_on = (AD_Value < 2000 ? 1 : 0);
         uint32_t currentTime = RTC_GetCounter(); /** 获取当前时间 **/
-
-        // 如果时间差超过 5 秒
-        if ((currentTime - startTime) >= 5)
+        if ((currentTime - startTime) >= 1)
         {
-            OLED_ShowNum(1, 1, currentTime, 8);
-            if (led_mode == BREATHE)
-                led_mode = BLINK;
-            else
-                led_mode = BREATHE;
-
-            startTime = currentTime; /** 重置开始时间，等待下一次 5 秒 **/
+            startTime = currentTime;
+            OLED_ShowNum(1, 5, AD_Value, 4);
         }
 
         if (led_mode == BREATHE)
